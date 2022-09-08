27b6d0391989ba59c7bb6cb0820bbe32920a3994
Artur Zaripov <mithotyn@mail.ru>
2022-09-08 22:49:00 +0300

    Lambda

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 class Main {
     public static void main(String[] args) {
-        PersonComparator pcomp = new PersonComparator(5);
+        Comparator<Person> pComp;
         ArrayList<Person> personList = new ArrayList<Person>();
         personList.add(new Person("Thomas", "Jefferson Murdok III", 29));
         personList.add(new Person("Thomas", "Jefferson Murdok Levy IV", 30));
@@ -15,7 +16,18 @@ class Main {
         for (int i = 0; i < personList.size(); i++)
             System.out.println(personList.get(i));
 
-        Collections.sort(personList, pcomp);
+        pComp = (Person p1, Person p2) ->
+        {
+            String[] surname1 = p1.getSurname().split(" ");
+            String[] surname2 = p2.getSurname().split(" ");
+            if (surname1.length >= 3 && surname2.length >= 3) {
+                return p1.compareTo(p2);
+            }
+            int last = Integer.compare(surname1.length, surname2.length);
+            return last == 0 ? p1.compareTo(p2) : last;
+        };
+
+        Collections.sort(personList, pComp);
 
         System.out.println("\nСписок лиц по знатности:");
         for (int i = 0; i < personList.size(); i++)
